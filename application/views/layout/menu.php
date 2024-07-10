88ca563a237f99b56fda88dac546c05e9ff2f1a2
janhuszarik <assist04@styx.at>
2024-07-10 15:50:08 +0200

    hjk

--- application/views/layout/menu.php
+++ application/views/layout/menu.php
@@ -88,14 +88,6 @@
 										</ul>
 									</nav>
 								</div>
-
-								<?php foreach (getLanguages() as $l): ?>
-									<li>
-										<a href="<?= BASE_URL . $l ?>">
-											<img src="<?= langInfo($l)['flag'] ?>" width="28px" alt="">
-										</a>
-									</li>
-								<?php endforeach; ?>
 								<ul class="header-social-icons social-icons d-none d-sm-block">
 									<li class="social-icons-facebook"><a href="http://www.facebook.com/" target="_blank" title="Facebook"><i class="fab fa-facebook-f"></i></a></li>
 									<li class="social-icons-twitter"><a href="http://www.twitter.com/" target="_blank" title="Twitter"><i class="fab fa-twitter"></i></a></li>
