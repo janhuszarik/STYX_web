951b41c7e2af19390d319f076f77d8eb677464dc
janhuszarik <assist04@styx.at>
2024-07-18 08:52:02 +0200

    klô

--- application/views/layout/menu.php
+++ application/views/layout/menu.php
@@ -1,32 +1,7 @@
 <body data-plugin-page-transition>
 <div class="body">
-	<header id="header" data-plugin-options='{"stickyEnabled": true, "stickyEnableOnBoxed": true, "stickyEnableOnMobile": false, "stickyStartAt": 45, "stickySetTop": "-45px", "stickyChangeLogo": true}'>
-		<div class="contact-info d-none d-lg-block" style="border-bottom: 1px solid #ddd; padding-bottom: 10px;">
-			<div class="container">
-				<div class="row">
-					<div class="col">
-						<ul class="header-social-icons social-icons">
-							<li><a href="https://www.facebook.com/www.styx.at/" target="_blank" title="Facebook"><i class="fab fa-facebook-f"></i></a></li>
-							<li><a href="https://www.instagram.com/styx.naturcosmetic/" target="_blank" title="Instagram"><i class="fab fa-instagram"></i></a></li>
-							<li><a href="https://at.pinterest.com/styxnaturcosmetic/" target="_blank" title="Pinterest"><i class="fab fa-pinterest"></i></a></li>
-							<li><a href="https://www.youtube.com/channel/UCxpLOYYahcENUfVnacBvagA" target="_blank" title="YouTube"><i class="fab fa-youtube"></i></a></li>
-							<li><a href="mailto:info@styx.at" target="_blank" title="Email"><i class="fa fa-envelope"></i></a></li>
-						</ul>
-					</div>
-					<div class="col">
-						<div class="nav-item nav-item-anim-icon d-none d-sm-block">
-							<div style="padding: 5px" class="d-flex">
-								<?php foreach (getLanguages() as $l) { ?>
-									<div class="active lang me-2">
-										<a href="<?= BASE_URL . $l ?>"><img src="<?= langInfo($l)['flag'] ?>" width="40px" alt=""></a>
-									</div>
-								<?php } ?>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
+	<header id="header" data-plugin-options="{'stickyEnabled': true, 'stickyEnableOnBoxed': true, 'stickyEnableOnMobile': false, 'stickyStartAt': 45, 'stickySetTop': '-45px', 'stickyChangeLogo': true}">
+		
 		<div class="header-body">
 			<div class="header-container container">
 				<div class="header-row">
@@ -34,7 +9,7 @@
 						<div class="header-row">
 							<div class="header-logo">
 								<a href="<?=BASE_URL?>">
-									<img alt="STYX Logo" width="180" height="80" data-sticky-width="102" data-sticky-height="40" data-sticky-top="40" src="<?=BASE_URL.LOGO?>">
+									<img alt="STYX Logo" width="180" height="80" data-sticky-width="102" data-sticky-height="40" data-sticky-top="25" src="<?=BASE_URL.LOGO?>">
 								</a>
 							</div>
 						</div>
@@ -43,6 +18,15 @@
 						<div class="header-row pt-3">
 							<nav class="header-nav-top">
 								<ul class="nav nav-pills">
+									<li class="nav-item dropdown nav-item-left-border d-none d-sm-block nav-item-left-border-remove nav-item-left-border-md-show">
+										<div style="padding: 5px" class="d-flex">
+											<?php foreach (getLanguages() as $l){ ?>
+												<div class="active lang me-2">
+													<a href="<?= BASE_URL . $l ?>"><img src="<?= langInfo($l)['flag'] ?>" width="40px" alt=""></a>
+												</div>
+											<?php } ?>
+										</div>
+									</li>
 									<li class="nav-item nav-item-left-border nav-item-left-border-remove nav-item-left-border-sm-show nav-item-hide">
 										<div class="container">
 											<a href="<?=lang('HÄNDLER_URL')?>"><button class="aroma-button">
@@ -53,7 +37,7 @@
 									<li class="nav-item nav-item-left-border nav-item-left-border-remove nav-item-left-border-sm-show nav-item-hide">
 										<div class="container">
 											<a href="https://shop.styx.at/"><button class="pulse-button">
-													<span style="font-weight: bold">SHOP</span>
+													<span style="font-weight: bold" </span> SHOP
 												</button></a>
 										</div>
 									</li>
@@ -112,6 +96,13 @@
 										</ul>
 									</nav>
 								</div>
+								<ul class="header-social-icons social-icons d-none d-sm-block">
+									<li class="social-icons-facebook"><a href="https://www.facebook.com/www.styx.at/" target="_blank" title="Facebook"><i class="fab fa-facebook-f"></i></a></li>
+									<li class="social-icons-instagram"><a href="https://www.instagram.com/styx.naturcosmetic/" target="_blank" title="Instagram"><i class="fab fa-instagram"></i></a></li>
+									<li class="social-icons-pinterest"><a href="https://at.pinterest.com/styxnaturcosmetic/" target="_blank" title="Linkedin"><i class="fab fa-pinterest"></i></a></li>
+									<li class="social-icons-youtube"><a href="https://www.youtube.com/channel/UCxpLOYYahcENUfVnacBvagA" target="_blank" title="Linkedin"><i class="fab fa-youtube"></i></a></li>
+									<li class="social-icons-email"><a href="mailto:info@styx.at" target="_blank" title="email"><i class="fa fa-envelope"></i></a></li>
+								</ul>
 								<button class="btn header-btn-collapse-nav" data-bs-toggle="collapse" data-bs-target=".header-nav-main nav">
 									<i class="fas fa-bars"></i>
 								</button>
