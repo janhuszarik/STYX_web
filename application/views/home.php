11fa0e443bdf2357d03165d280b80a07788c2b97
janhuszarik <assist04@styx.at>
2024-07-19 11:51:17 +0200

    cv

--- application/views/home.php
+++ application/views/home.php
@@ -93,47 +93,105 @@
 			</div>
 		</div>
 	</div>
+	<style>
+		.outer-card {
+			padding: 5px; /* Priestor okolo vnútorných kariet */
+			border-radius: 8px; /* Zaoblené rohy pre vonkajšiu kartu */
+			box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1); /* Jemný tieň pre lepší vzhľad */
+		}
 
+		.inner-card .card {
+			border-radius: 0; /* Odstránime zaoblené rohy vnútorných kariet */
+			margin-bottom: 0; /* Odstránime spodný okraj prvej vnútornej karty */
+		}
+
+		.inner-card .card:first-child {
+			border-bottom-left-radius: 0; /* Odstránime dolné ľavé zaoblenie prvej vnútornej karty */
+			border-bottom-right-radius: 0; /* Odstránime dolné pravé zaoblenie prvej vnútornej karty */
+		}
+
+		.inner-card .card:last-child {
+			border-top-left-radius: 0; /* Odstránime horné ľavé zaoblenie druhej vnútornej karty */
+			border-top-right-radius: 0; /* Odstránime horné pravé zaoblenie druhej vnútornej karty */
+			margin-top: -1px; /* Prekryjeme horný okraj druhej vnútornej karty */
+		}
+
+		.d-flex.justify-content-between > .card-wrapper {
+			margin-right: 1rem; /* Pridáme medzeru medzi kartami */
+		}
+
+		.d-flex.justify-content-between > .card-wrapper:last-child {
+			margin-right: 0; /* Posledná karta nemá pravý margin */
+		}
+
+
+
+	</style>
 	<div class="container">
-		<div class="featured-boxes py-5 mt-5 mb-4">
-			<div class="row">
-				<div class="col-lg-3 col-sm-6">
-					<div class="featured-box featured-box-primary featured-box-effect-1">
-						<div class="box-content">
-							<i class="icon-featured icons icon-people"></i>
-							<h3 class="text-color-primary font-weight-bold text-3 mb-2 mt-3">Loved by Customers</h3>
-							<p class="px-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-							<p><a href="/" class="text-dark learn-more font-weight-bold text-2">VIEW MORE <i class="fas fa-chevron-right ms-2"></i></a></p>
+		<div class="d-flex justify-content-between">
+			<div class="card-wrapper col-md-6 col-lg-5 mb-5 mb-lg-0 appear-animation" data-appear-animation="fadeInUpShorter" data-appear-animation-delay="200">
+				<div class="outer-card card">
+					<div class="inner-card">
+						<div class="card mb-3">
+							<div class="row g-0">
+								<div class="col-lg-4 d-flex align-items-center justify-content-center">
+									<img src="<?=BASE_URL.'img/webImage/naturcosmeticImage.jpg'?>" class="img-fluid rounded-start" alt="...">
+								</div>
+								<div class="col-lg-8">
+									<div class="card-body">
+										<h4 class="card-title mb-1 text-7 font-weight-bold">NATURCOSMETIC</h4>
+										<a href="/" class="read-more text-color-primary font-weight-semibold text-2">Read More <i class="fas fa-angle-right position-relative top-1 ms-1"></i></a>
+									</div>
+								</div>
+							</div>
+						</div>
+
+						<div class="card mb-3">
+							<div class="row g-0">
+								<div class="col-lg-8">
+									<div class="card-body">
+										<h4 class="card-title mb-1 text-4 font-weight-bold"></h4>
+										<p class="card-text mb-2">Naturkosmetik aus Österreich. Viele zertifizierte und vegane Produkte.</p>
+									</div>
+								</div>
+								<div class="col-lg-4 d-flex align-items-center justify-content-center">
+									<img src="<?=BASE_URL.'img/webImage/greenLogo.jpg'?>" class="img-fluid rounded-start" alt="...">
+								</div>
+							</div>
 						</div>
 					</div>
 				</div>
-				<div class="col-lg-3 col-sm-6">
-					<div class="featured-box featured-box-dark featured-box-effect-1">
-						<div class="box-content">
-							<i class="icon-featured icons icon-docs"></i>
-							<h3 class="font-weight-bold text-3 mb-2 mt-3">Well Documented</h3>
-							<p class="px-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-							<p><a href="/" class="text-dark learn-more font-weight-bold text-2">VIEW MORE <i class="fas fa-chevron-right ms-2"></i></a></p>
+			</div>
+
+			<div class="card-wrapper col-md-6 col-lg-5 mb-5 mb-lg-0 appear-animation" data-appear-animation="fadeInUpShorter" data-appear-animation-delay="200">
+				<div class="outer-card card">
+					<div class="inner-card">
+						<div class="card mb-3">
+							<div class="row g-0">
+								<div class="col-lg-4 d-flex align-items-center justify-content-center">
+									<img src="<?=BASE_URL.'img/webImage/naturcosmeticImage.jpg'?>" class="img-fluid rounded-start" alt="...">
+								</div>
+								<div class="col-lg-8">
+									<div class="card-body">
+										<h4 class="card-title mb-1 text-7 font-weight-bold">NATURCOSMETIC</h4>
+										<a href="/" class="read-more text-color-primary font-weight-semibold text-2">Read More <i class="fas fa-angle-right position-relative top-1 ms-1"></i></a>
+									</div>
+								</div>
+							</div>
 						</div>
-					</div>
-				</div>
-				<div class="col-lg-3 col-sm-6">
-					<div class="featured-box featured-box-primary featured-box-effect-1">
-						<div class="box-content">
-							<i class="icon-featured icons icon-trophy"></i>
-							<h3 class="text-color-primary font-weight-bold text-3 mb-2 mt-3">Winner</h3>
-							<p class="px-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-							<p><a href="/" class="text-dark learn-more font-weight-bold text-2">VIEW MORE <i class="fas fa-chevron-right ms-2"></i></a></p>
-						</div>
-					</div>
-				</div>
-				<div class="col-lg-3 col-sm-6">
-					<div class="featured-box featured-box-dark featured-box-effect-1">
-						<div class="box-content">
-							<i class="icon-featured icons icon-equalizer"></i>
-							<h3 class="font-weight-bold text-3 mb-2 mt-3">Customizable</h3>
-							<p class="px-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-							<p><a href="/" class="text-dark learn-more font-weight-bold text-2">VIEW MORE <i class="fas fa-chevron-right ms-2"></i></a></p>
+
+						<div class="card mb-3">
+							<div class="row g-0">
+								<div class="col-lg-8">
+									<div class="card-body">
+										<h4 class="card-title mb-1 text-4 font-weight-bold"></h4>
+										<p class="card-text mb-2">Naturkosmetik aus Österreich. Viele zertifizierte und vegane Produkte.</p>
+									</div>
+								</div>
+								<div class="col-lg-4 d-flex align-items-center justify-content-center">
+									<img src="<?=BASE_URL.'img/webImage/greenLogo.jpg'?>" class="img-fluid rounded-start" alt="...">
+								</div>
+							</div>
 						</div>
 					</div>
 				</div>
@@ -141,120 +199,5 @@
 		</div>
 	</div>
 
-	<section class="section section-height-3 section-background border-0 m-0 appear-animation" data-appear-animation="fadeIn" style="background-image: url(img/parallax/parallax-10.jpg); background-size: cover; background-position: center;">
-		<div class="container">
-			<div class="row">
-				<div class="col appear-animation" data-appear-animation="fadeInUpShorter" data-appear-animation-delay="200">
 
-					<div class="owl-carousel owl-theme nav-bottom rounded-nav" data-plugin-options="{'items': 1, 'loop': false}">
-						<div>
-							<div class="testimonial testimonial-style-2 testimonial-with-quotes testimonial-quotes-dark mb-0">
-								<div class="testimonial-author">
-									<img src="img/clients/client-1.jpg" class="img-fluid rounded-circle" alt="">
-								</div>
-								<blockquote>
-									<p class="text-color-dark text-5 line-height-5 mb-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eget risus porta, tincidunt turpis at, interdum tortor. Suspendisse potenti. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Fusce ante tellus, convallis non consectetur sed, pharetra nec ex.</p>
-								</blockquote>
-								<div class="testimonial-author">
-									<p><strong class="font-weight-extra-bold text-2">- John Smith. Okler</strong></p>
-								</div>
-							</div>
-						</div>
-						<div>
-							<div class="testimonial testimonial-style-2 testimonial-with-quotes testimonial-quotes-dark mb-0">
-								<div class="testimonial-author">
-									<img src="img/clients/client-1.jpg" class="img-fluid rounded-circle" alt="">
-								</div>
-								<blockquote>
-									<p class="text-color-dark text-5 line-height-5 mb-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eget risus porta, tincidunt turpis at, interdum tortor. Suspendisse potenti. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-								</blockquote>
-								<div class="testimonial-author">
-									<p><strong class="font-weight-extra-bold text-2">- John Smith. Okler</strong></p>
-								</div>
-							</div>
-						</div>
-					</div>
-
-				</div>
-			</div>
-		</div>
-	</section>
-
-	<div class="container">
-		<div class="row pt-5 mt-4">
-			<div class="col">
-				<h2 class="font-weight-normal text-6 mb-4"><strong class="font-weight-extra-bold">Latest</strong> Posts</h2>
-			</div>
-		</div>
-		<div class="row recent-posts pb-5 mb-4">
-			<div class="col-md-6 col-lg-3 mb-4 mb-lg-0">
-				<article>
-					<div class="row">
-						<div class="col-auto pe-0">
-							<div class="date">
-								<span class="day text-color-dark font-weight-extra-bold">15</span>
-								<span class="month text-1">JAN</span>
-							</div>
-						</div>
-						<div class="col ps-1">
-							<h4 class="line-height-3 text-4"><a href="blog-post.html" class="text-dark">Lorem ipsum dolor sit amet, consectetur</a></h4>
-							<p class="line-height-5 pe-4 mb-1">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-							<a href="/" class="read-more text-color-dark font-weight-bold text-2">read more <i class="fas fa-chevron-right text-1 ms-1"></i></a>
-						</div>
-					</div>
-				</article>
-			</div>
-			<div class="col-md-6 col-lg-3 mb-4 mb-lg-0">
-				<article>
-					<div class="row">
-						<div class="col-auto pe-0">
-							<div class="date">
-								<span class="day text-color-dark font-weight-extra-bold">14</span>
-								<span class="month text-1">JAN</span>
-							</div>
-						</div>
-						<div class="col ps-1">
-							<h4 class="line-height-3 text-4"><a href="blog-post.html" class="text-dark">Lorem ipsum dolor sit amet, consectetur</a></h4>
-							<p class="line-height-5 pe-4 mb-1">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-							<a href="/" class="read-more text-color-dark font-weight-bold text-2">read more <i class="fas fa-chevron-right text-1 ms-1"></i></a>
-						</div>
-					</div>
-				</article>
-			</div>
-			<div class="col-md-6 col-lg-3 mb-4 mb-md-0">
-				<article>
-					<div class="row">
-						<div class="col-auto pe-0">
-							<div class="date">
-								<span class="day text-color-dark font-weight-extra-bold">13</span>
-								<span class="month text-1">JAN</span>
-							</div>
-						</div>
-						<div class="col ps-1">
-							<h4 class="line-height-3 text-4"><a href="blog-post.html" class="text-dark">Lorem ipsum dolor sit amet, consectetur</a></h4>
-							<p class="line-height-5 pe-4 mb-1">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-							<a href="/" class="read-more text-color-dark font-weight-bold text-2">read more <i class="fas fa-chevron-right text-1 ms-1"></i></a>
-						</div>
-					</div>
-				</article>
-			</div>
-			<div class="col-md-6 col-lg-3">
-				<article>
-					<div class="row">
-						<div class="col-auto pe-0">
-							<div class="date">
-								<span class="day text-color-dark font-weight-extra-bold">12</span>
-								<span class="month text-1">JAN</span>
-							</div>
-						</div>
-						<div class="col ps-1">
-							<h4 class="line-height-3 text-4"><a href="blog-post.html" class="text-dark">Lorem ipsum dolor sit amet, consectetur</a></h4>
-							<p class="line-height-5 pe-4 mb-1">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-							<a href="/" class="read-more text-color-dark font-weight-bold text-2">read more <i class="fas fa-chevron-right text-1 ms-1"></i></a>
-						</div>
-					</div>
-				</article>
-			</div>
-		</div>
-	</div>
 </div>
