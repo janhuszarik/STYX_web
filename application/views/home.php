f01b18048831f0e3be728cd9c2c33921e11779cb
janhuszarik <assist04@styx.at>
2024-07-22 13:56:45 +0200

    hg

--- application/views/home.php
+++ application/views/home.php
@@ -1,33 +1,35 @@
-<div class="home-intro light border border-bottom-0 mb-0">
-	<div class="container">
-		<div class="row">
-			<div class="col">
-				<h1 style='font-weight: bolder' class="text-center"><?=lang('AKTUELL')?></h1>
-				<div class="owl-carousel owl-theme show-nav-title show-nav-title-both-sides news-carousel" data-plugin-options="{'items': 4, 'margin': 10, 'loop': true, 'nav': true, 'dots': false, 'autoplay': false}">
-					<?php foreach ($news as $news_item): ?>
-						<div class="home-carousel-card" onclick="location.href='<?=$news_item->buttonUrl?>';">
-							<div class="home-carousel-img-container">
-								<img alt="" class="img-fluid rounded" src="<?=BASE_URL?>uploads/news/<?=$news_item->image?>">
+<div role="main" class="main">
+	<div class="home-intro light border border-bottom-0 mb-0">
+		<div class="container">
+			<div class="row">
+				<div class="col">
+					<h1 style='font-weight: bolder' class="text-center"><?=lang('AKTUELL')?></h1>
+					<div class="owl-carousel owl-theme show-nav-title show-nav-title-both-sides news-carousel" data-plugin-options="{'items': 4, 'margin': 10, 'loop': false, 'nav': true, 'dots': false, 'autoplay': true}">
+						<?php foreach ($news as $news_item): ?>
+							<div class="home-carousel-card" onclick="location.href='<?=$news_item->buttonUrl?>';" style="cursor: pointer;">
+								<div class="home-carousel-img-container">
+									<img alt="" class="img-fluid rounded" src="<?=BASE_URL?>uploads/news/<?=$news_item->image?>">
+								</div>
+								<div class="home-carousel-card-content">
+									<h5><?=$news_item->name?></h5>
+									<p><?=$news_item->name1?></p>
+								</div>
 							</div>
-							<div class="home-carousel-card-content">
-								<h5><?=$news_item->name?></h5>
-								<p><?=$news_item->name1?></p>
-							</div>
-						</div>
-					<?php endforeach; ?>
+						<?php endforeach; ?>
+					</div>
 				</div>
 			</div>
 		</div>
 	</div>
-</div>
 
-<div class="row">
-	<div class="col">
-		<hr class="solid my-5">
-		<h1 style='font-weight: bolder' class="text-center"><?=lang('PRODUCT_WEB')?></h1>
+	<div class="row">
+		<div class="col">
+			<hr class="solid my-5">
+			<h1 style='font-weight: bolder' class="text-center"><?=lang('PRODUCT_WEB')?></h1>
+		</div>
 	</div>
-</div>
 
+<<<<<<< HEAD
 <br>
 <div id="product-carousel" class="owl-carousel owl-theme full-width" data-plugin-options='{"items": 6, "loop": true, "nav": true, "dots": false, "autoplay": true, "autoplayTimeout": 1, "autoplayHoverPause": false}'>
 	<?php foreach ($product as $product_item): ?>
@@ -35,43 +37,41 @@
             <span class="thumb-info thumb-info-centered-info thumb-info-no-borders">
                 <span class="thumb-info-wrapper">
                     <img src="<?=BASE_URL?>uploads/product/<?=$product_item->image?>" class="img-fluid" alt="<?=$product_item->name?>">
+=======
+	<br>
+	<div class="owl-carousel owl-theme full-width" data-plugin-options="{'items': 6, 'loop': true, 'nav': true, 'dots': false}">
+		<?php foreach ($product as $product_item): ?>
+			<a href="<?=$product_item->url?>" aria-label="">
+                <span class="thumb-info thumb-info-centered-info thumb-info-no-borders">
+                    <span class="thumb-info-wrapper">
+                        <img src="<?=BASE_URL?>uploads/product/<?=$product_item->image?>" class="img-fluid" alt="<?=$product_item->name?>">
+                    </span>
+>>>>>>> parent of 016ad42 (správne nastavenie)
                 </span>
-            </span>
-			<?php if ($product_item->action == 1): ?>
-				<div class="ribbon">
-					<?php if (!empty($product_item->aktion_name) && !empty($product_item->price)): ?>
-						<?=$product_item->aktion_name?> / <?=$product_item->price?>
-					<?php elseif (!empty($product_item->aktion_name)): ?>
-						<?=$product_item->aktion_name?>
-					<?php elseif (!empty($product_item->price)): ?>
-						<?=$product_item->price?>
-					<?php endif; ?>
+				<?php if ($product_item->action == 1): ?>
+					<div class="ribbon">
+						<?php if (!empty($product_item->aktion_name) && !empty($product_item->price)): ?>
+							<?=$product_item->aktion_name?> / <?=$product_item->price?>
+						<?php elseif (!empty($product_item->aktion_name)): ?>
+							<?=$product_item->aktion_name?>
+						<?php elseif (!empty($product_item->price)): ?>
+							<?=$product_item->price?>
+						<?php endif; ?>
+					</div>
+				<?php endif; ?>
+				<div class="product-info">
+					<?= $product_item->name ?>
 				</div>
-			<?php endif; ?>
-			<div class="product-info">
-				<?= $product_item->name ?>
-			</div>
-		</a>
-	<?php endforeach; ?>
-</div>
-<script>
-	$(document).ready(function(){
-		$("#product-carousel").owlCarousel({
-			items: 6,
-			loop: true,
-			nav: true,
-			dots: false,
-			autoplay: true,
-			autoplayTimeout: 5000,
-			autoplayHoverPause: true,
-			autoplaySpeed: 1500,
-			slideTransition: 'linear',
-		});
-	});
-</script>
+			</a>
+		<?php endforeach; ?>
+	</div>
 
+<<<<<<< HEAD
 
 <div class="container py-5 my-4">
+=======
+	<div class="container py-5 my-4">
+>>>>>>> parent of 016ad42 (správne nastavenie)
 		<div class="row text-center py-3">
 			<div class="col-md-10 mx-md-auto">
 				<h5 class="text-uppercase mt-4"><?=lang('FIRMA_LOGOS')?></h5>
