c115211c67fec859c275b9482ef1cd18c9dfdde1
janhuszarik <assist04@styx.at>
2024-07-25 09:30:24 +0200

    slider texty

--- application/views/layout/slider.php
+++ application/views/layout/slider.php
@@ -10,17 +10,17 @@
 						<?php if ($hasText): ?>
 							<div class="text-background">
 								<?php if (!empty($s->name1)): ?>
-									<h1 class="text-color-dark text-5 line-height-5 font-weight-semibold px-4 mb-2 appear-animation" data-appear-animation="fadeInDownShorterPlus" data-plugin-options="{'minWindowWidth': 0}">
+									<h1 class="text-color-dark text-4 line-height-5 font-weight-semibold px-4 mb-2 appear-animation" data-appear-animation="fadeInDownShorterPlus" data-plugin-options="{'minWindowWidth': 0}">
 										<span class="position-absolute right-100pct top-50pct transform3dy-n50"></span>
 										<?php echo $s->name1; ?>
 										<span class="position-absolute left-100pct top-50pct transform3dy-n50"></span>
 									</h1>
 								<?php endif; ?>
 								<?php if (!empty($s->name2)): ?>
-									<h1 class="text-color-dark font-weight-extra-bold text-6 mb-3 appear-animation" data-appear-animation="blurIn" data-appear-animation-delay="1000" data-plugin-options="{'minWindowWidth': 0}"><?php echo $s->name2; ?></h1>
+									<h1 class="text-color-dark font-weight-extra-bold text-5 mb-3 line-height-2 appear-animation" data-appear-animation="blurIn" data-appear-animation-delay="1000" data-plugin-options="{'minWindowWidth': 0}"><?php echo $s->name2; ?></h1>
 								<?php endif; ?>
 								<?php if (!empty($s->name3)): ?>
-									<p class="text-5 text-color-dark font-weight-light mb-0" data-plugin-animated-letters data-plugin-options="{'startDelay': 2000, 'minWindowWidth': 0}"><?php echo $s->name3; ?></p>
+									<p class="text-4 text-color-dark font-weight-light mb-0" data-plugin-animated-letters data-plugin-options="{'startDelay': 2000, 'minWindowWidth': 0}"><?php echo $s->name3; ?></p>
 								<?php endif; ?>
 								<?php if (!empty($s->button_text) && !empty($s->button_link)): ?>
 									<a href="<?php echo $s->button_link; ?>" class="btn btn-success mt-3 animate__animated"><?php echo $s->button_text; ?></a>
