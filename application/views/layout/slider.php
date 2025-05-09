35eefede7cc7ae33a15049f32f7b570df29ad8d8
Jan Huszarik <assist04@styx.at>
2025-05-09 10:34:35 +0200

    df

--- application/views/layout/slider.php
+++ application/views/layout/slider.php
@@ -5,30 +5,27 @@
 				<div class="slider-wrapper <?= $index === 0 ? 'active' : '' ?>">
 
 					<!-- Obrázkový slider -->
-					<div class="slider-section" style="background-image: url('<?php echo base_url('uploads/sliders/' . $s->image); ?>');">
+					<div class="slider-section" style="background-image: url('<?= base_url('uploads/sliders/' . $s->image); ?>');">
 						<!-- Navigácia -->
 						<div class="slider-navigation">
-							<span class="prev">&#10094;</span>
-							<span class="next">&#10095;</span>
+							<span class="prev">❮</span>
+							<span class="next">❯</span>
 						</div>
 					</div>
 
+					<!-- Textová lišta POD sliderom -->
 					<?php $textColor = 'color:' . htmlspecialchars($s->text_color); ?>
 					<a href="<?= !empty($s->button_link) ? $s->button_link : '#' ?>" style="text-decoration: none;">
 						<div class="slider-text<?= (empty($s->title) && empty($s->name1) && empty($s->name2) && empty($s->name3)) ? ' empty' : ''; ?>"
 							 style="background-color: <?= htmlspecialchars($s->bg_color); ?>;">
-
 							<?php if (!empty($s->title)): ?><h2 style="<?= $textColor ?>"><?= $s->title ?></h2><?php endif; ?>
 							<?php if (!empty($s->name1)): ?><h3 style="<?= $textColor ?>"><?= $s->name1 ?></h3><?php endif; ?>
 							<?php if (!empty($s->name2)): ?><p style="<?= $textColor ?>"><?= $s->name2 ?></p><?php endif; ?>
 							<?php if (!empty($s->name3)): ?><p style="<?= $textColor ?>"><?= $s->name3 ?></p><?php endif; ?>
 						</div>
 					</a>
-
-
 				</div>
 			<?php endforeach; ?>
 		</div>
 	<?php endif; ?>
 </div>
-
