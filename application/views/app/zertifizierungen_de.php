6815a0f9c1c7eb34c2e08020f11a4a197c8ca1f8
Jan Huszarik <assist04@styx.at>
2025-05-26 11:52:08 +0200

    gg

--- application/views/app/zertifizierungen_de.php
+++ application/views/app/zertifizierungen_de.php
@@ -1,126 +1,136 @@
+<section class="container py-5">
+	<h2 class="text-center mb-5">Unsere Zertifizierungen & Auszeichnungen</h2>
 
-
-<div role="main" class="main">
-	<section class="page-header page-header-modern page-header-background page-header-background-md overlay overlay-color-primary overlay-show overlay-op-9 mb-0" style="background-image: url(<?=$image1?>);">
-		<div class="container translucent-background">
-			<div class="row">
-				<div class="col align-self-center p-static text-center">
-					<h1 style="color:#000;"><?=$title?></h1>
-					<span style="color: #0a0a0a" class="sub-title"><?=$description?></span>
-				</div>
-			</div>
+	<section class="row mb-5 align-items-center">
+		<div class="col-md-9">
+			<h4 class="mb-2 text-success">Austria BIO Garantie</h4>
+			<p>Die Austria Bio Garantie GmbH (ABG) wurde im Jahr 1993 als Bio-Kontrollstelle gegründet.
+				Aufgabe ist die Kontrolle und Zertifizierung biologischer Produkte: vom biologischen Urprodukt bis hin zum Letztverarbeiter.
+				Die ABG arbeitet im Auftrag der Lebensmittelbehörden. Produkte, die von der ABG zertifiziert werden, tragen die Codenummer AT-BIO-301.
+				Viele unserer handgemachten <a href="<?= BASE_URL . language() . '/Company/Chocolate-manufactory' ?>">Schokoladen,</a>  sowie
+				<a href="https://shop.styx.at/Naturkosmetik/Koerperpflege/Koerpercreme:::3_75_78.html">Hanf und Mohn</a> Pflegeprodukte tragen diese BIO-Kennzeichnung.</p>
+			<a href="https://www.bio-garantie.at/de/home" class="btn btn-outline-success btn-sm">Mehr Informationen</a>
+		</div>
+		<div class="col-md-3 text-center">
+			<img src="img/cert/Austria_Bio_Garantie.png" class="img-fluid" alt="Austria BIO Garantie">
 		</div>
 	</section>
-	<section class="page-header bg-color-light border-bottom border-width-2 translucent-background">
-		<div class="container">
-			<div class="row">
-				<div class="col align-self-center p-static">
-					<ul class="breadcrumb d-block">
-						<li><a href="<?=BASE_URL?>">Home</a></li>
-						<li class="active"><?=$title?></li>
-					</ul>
-				</div>
-			</div>
+
+	<section class="row mb-5 align-items-center">
+		<div class="col-md-9">
+			<h4 class="mb-2 text-success">ICADA Zertifizierung</h4>
+			<p>ICADA (International Cosmetic and Device Association) ist ein europäischer Service- und Lobby-Verband im Bereich präparative Kosmetik, apparative Kosmetik,
+				natürliche Wasch- und Reinigungsmittel sowie Nahrungsergänzungs- und Lebensmittel. ICADA bietet mit ihrer über 20 jährigen Erfahrungen ein Premium-Zeichen
+				für Bio- und Naturkosmetik, das durch das Doppelkontrollsystem besonders streng und zuverlässig ist, und sich durch die Erlaubnis relativ weniger Zusatzstoffe
+				(kurze Positivliste) nah an der Natur ausrichtet.</p>
+			<a href="https://www.icada.eu/" class="btn btn-outline-success btn-sm">Prüfkriterien</a>
+		</div>
+		<div class="col-md-3 text-center">
+			<img src="img/cert/ICADA.png" class="img-fluid" alt="ICADA">
 		</div>
 	</section>
-</div>
 
-<div class="container py-4">
-
-	<div class="row">
-		<div class="col">
-			<div class="blog-posts single-post">
-
-				<article class="post post-large blog-single-post border-0 m-0 p-0">
-
-					<div class="post-content ms-0">
-
-						<h2 style="color: #aad998" class="font-weight-semi-bold">Hand in Hand mit der Natur</h2>
-
-						<div class="post-meta">
-							<h4 class="text-color-black">Traditionelles Familienunternehmen seit 1965 – im Herzen Niederösterreichs</h4>
-						</div>
-						<img src="<?=BASE_URL?>img/image/aboutStyx.jpg" class="img-fluid float-start me-4 mt-2" alt="" /><br>
-
-						<p> Inmitten geschwungener Hügel und grüner Wiesen erstreckt sich das niederösterreichische Pielachtal.
-							Ein ruhiger und beschaulicher Ort, der zum Verweilen und Träumen einlädt. Zugleich ein Ort, der die
-							Herzen all derjenigen erfreut, die sich für naturkosmetische Pflege begeistern. Hier im Innern dieser
-							einladenden Landschaft liegt Ober-Grafendorf und Kundige wissen, dort findet sich ein Platz ganz besonderer Art.
-							Dort, wo sich Tradition und Moderne vereinen, liegt die Heimat von STYX Naturcosmetic.
-							Seit 1965 und damit seit mehr als 55 Jahren widmet sich das österreichische Familienunternehmen mit mittlerweile
-							rund 60 Mitarbeitern der Herstellung von Naturkosmetik. Was sich nach erfahrungsreichem Handwerk anhört, ist bei
-							STYX Naturcosmetic noch weit mehr. Für uns führt der Prozess, in dessen Verlauf Naturkosmetik entsteht über das
-							Handwerkliche hinaus. Für den, der wahre Naturkosmetik erzeugen will, in der sich Philosophie, Qualität und
-							Leidenschaft des Unternehmens spiegeln, bedeutet dieser Prozess nicht nur Handwerk, sondern Kunst. Die Kunst
-							einer unvergleichlichen natürlichen Pflege.
-							<br>
-							Diesen hohen Anspruch möchten wir aber nicht nur in unseren Produkten erfahrbar machen. Wer sich für die Herstellung von Naturkosmetik interessiert ist herzlich eingeladen uns im Pielachtal zu besuchen. Täglich bieten wir unseren Besuchern die Möglichkeit vieles rund ums Thema Naturkosmetik authentisch und hautnah zu erfahren. Unsere hellen und verglasten Produktionshallen gewähren trotz strenger Hygieneauflagen interessante Einblicke in unsere tägliche Arbeit. Der hauseigene Naturkosmetik-Shop und ein gemütliches Bistro laden zum längeren Verweilen ein.</a>
-						</p>
-						<div class="ratio ratio-16x9">
-							<iframe frameborder="0" allowfullscreen="" src="https://www.youtube.com/embed/atSpOLLMDEY"></iframe>
-						</div>
-
-						<br><br>
-						<div class="post-meta">
-							<h4 class="text-color-black">Staatliche Auszeichnung (2003)</h4>
-						</div>
-						<img src="<?=BASE_URL?>img/image/Staatliche-Auszeichnung-rgb.jpg" width="200" class="img-fluid float-end me-4 mt-2" alt="Staatliche-Auszeichnung-rgb.jpg" /><br>
-
-						<p>Staatliche Auszeichnungen in Österreich werden durch den Bundesminister für Wirtschaft an Unternehmen verliehen,
-							die sich durch außergewöhnliche Leistungen um die österreichische Wirtschaft Verdienste erworben haben.
-							<br>Seit 2003 sind wir stolz, diese Auszeichnung tragen zu dürfen.
-						</p>
-						<br>
-						<div class="post-block mt-5 post-share">
-							<a href="https://www.staatswappen.at/" class="btn btn-primary w-50 mb-2">Mehr Information</a>
-
-							<!-- Go to www.addthis.com/dashboard to customize your tools -->
-							<div class="addthis_inline_share_toolbox"></div>
-							<script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-60ba220dbab331b0"></script>
-
-						</div>
-						<br>
-						<h4>Green Brand Austria Gütesiegel (2022/2023)</h4>
-						<img src="<?=BASE_URL?>img/image/Green Brands 2022_2023 STYX Naturcosmetic.jpg" width="200" class="img-fluid float-end me-4 mt-2" alt="Green Brands 2022_2023 STYX Naturcosmetic.jpg" /><br>
-
-						<p> Für unseren wertvollen Beitrag zum Umwelt- und Klimaschutz. Green Brands honoriert darüber Hinaus auch die
-							Verpflichtung zur Nachhaltigkeit und zur ökologischer Verantwortung
-						</p>
-						<br><br>
-						<div class="post-block mt-5 post-share">
-							<a href="https://https://green-brands.org/" class="btn btn-primary w-50 mb-2">Mehr Information</a>
-
-							<!-- Go to www.addthis.com/dashboard to customize your tools -->
-							<div class="addthis_inline_share_toolbox"></div>
-							<script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-60ba220dbab331b0"></script>
-
-						</div>
-
-						<br><br>
-
-						<h4>Klimabündnisbetrieb Österreich (2018)</h4>
-						<img src="<?=BASE_URL?>img/image/kbu_logos_betrieb-rgb.jpg" width="200" class="img-fluid float-end me-4 mt-2" alt="" /><br>
-
-						<p> Mit Freude sind wir die freiwillige Verpflichtung für aktive Maßnahmen zum nachhaltigen Klimaschutz eingegangen.
-							<br>
-							Das Klimabündnis ist eine globale Partnerschaft zum Schutz des Klimas und verbindet mittlerweile
-							<br> mehr als 1.700 Gemeinden in 26 Ländern Europas.
-						</p>
-						<br><br>
-						<div class="post-block mt-5 post-share">
-							<a href="https://www.klimabuendnis.at/" class="btn btn-primary w-50 mb-2">Mehr Information</a>
-
-							<!-- Go to www.addthis.com/dashboard to customize your tools -->
-							<div class="addthis_inline_share_toolbox"></div>
-							<script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-60ba220dbab331b0"></script>
-
-						</div>
-
-					</div>
-				</article>
-
-			</div>
+	<section class="row mb-5 align-items-center">
+		<div class="col-md-9">
+			<h4 class="mb-2 text-success">ECOCERT COSMOS</h4>
+			<p>Zertifizierungen ECOCERT ist eine internatioal anerkannte Organisation zur Zertifizierung von Lebensmitteln und Kosmetikartikeln. Die definierten Qualitätskriterien setzen einen Standard für Natur- und Biokosmetik.
+				STYX Naturcosmetic arbeitet seit 2011 eng mit ECOCERT zusammen, um höchste Qualitätsstandards zu garantieren. Wir sind bestrebt im laufenden Prozess alle Produkte einer BIO Zertifizierung zuzuführen.
+				Bereits zertifizierte Produkte sind anhand des ECOCERT Logos auf unseren Produkten zu erkennen.</p>
+			<a href="https://www.ecocert.com/de-DE/home" class="btn btn-outline-success btn-sm">Prüfkriterien</a>
 		</div>
-	</div>
+		<div class="col-md-3 text-center">
+			<img src="img/cert/ECOCERT.png" class="img-fluid" alt="ECOCERT COSMOS">
+		</div>
+	</section>
 
-</div>
+	<section class="row mb-5 align-items-center">
+		<div class="col-md-9">
+			<h4 class="mb-2 text-success">Produktkennzeichnung VEGAN</h4>
+			<p>Unser Vegan-Logo lässt auf einen Blick erkennen, welche STYX Produkte zu 100 % frei von tierischen Inhaltsstoffen sind. Ein veganes Produkt ist gänzlich frei von allem Tierischen. Veganismus ist Überzeugung und Lebensphilosophie.</p>
+		</div>
+		<div class="col-md-3 text-center">
+			<img src="img/cert/Vegan.png" class="img-fluid" alt="VEGAN">
+		</div>
+	</section>
+
+	<section class="row mb-5 align-items-center">
+		<div class="col-md-9">
+			<h4 class="mb-2 text-success">Trusted Shops</h4>
+			<p>Trusted Shops ist das führende Gütesiegel für Online-Shops in Europa mit einem Käuferschutz für Verbraucher und dem Bewertungssystem für Online-Shops.
+				Seit 2017 ist unser <a href="https://shop.styx.at">Webshop</a> mit diesem Gütesigel zertifiziert. Damit erfüllen wir alle Qualitätskriterien in Bezug auf Datensicherheit, transparenter
+				Kundenkommunikation und unserem verlässlichen Kundenservice. Bei einer Anmeldung können Sie sich für Ihre Einkäufe von bis zu 100€ absichern.</p>
+			<a href="https://shop.styx.at" class="btn btn-outline-success btn-sm">Zum Shop</a>
+		</div>
+		<div class="col-md-3 text-center">
+			<img src="img/cert/Trusted_Shops.png" class="img-fluid" alt="Trusted Shops">
+		</div>
+	</section>
+
+	<section class="row mb-5 align-items-center">
+		<div class="col-md-9">
+			<h4 class="mb-2 text-success">Green Brand Austria Gütesiegel (2022 / 2023)</h4>
+			<p>Bereits 2014 haben wir für unseren wertvollen <a href="<?=BASE_URL.language().'/Unternehmen/Philosophie'?>">Beitrag zum Umwelt- und Klimaschutz</a> das Green Brands verliehen bekommen. Nach 2018/2019 haben wir dieses Versprechen 2022 / 2023 erneuert.
+				GREEN BRANDS sind mit dem Gütesiegel ausgezeichnete Marken (Unternehmen / Produkte / Dienstleister / Lebensmittel und Persönlichkeiten), die einen maßgeblichen Beitrag zum Schutz der Umwelt, der Natur und des Klimas leisten.</p>
+			<a href="#" class="btn btn-outline-success btn-sm">Mehr Informationen</a>
+		</div>
+		<div class="col-md-3 text-center">
+			<img src="img/cert/Green_Brand.png" class="img-fluid" alt="Green Brand Austria">
+		</div>
+	</section>
+
+	<section class="row mb-5 align-items-center">
+		<div class="col-md-9">
+			<h4 class="mb-2 text-success">Das AUSTRIA Zeichen (WKO)</h4>
+			<p>Das Austria Zeichen ist eine starke Marke, die im In-und Ausland seit über 30 Jahren als Symbol für die österreichische Wirtschaft steht.
+				Es steht für die Qualität und die Innovationskraft österreichischer Produkte.</p>
+			<a href="https://www.wko.at/aussenwirtschaft/austria-zeichen" class="btn btn-outline-success btn-sm">Mehr Informationen</a>
+		</div>
+		<div class="col-md-3 text-center">
+			<img src="img/cert/Austria_WKO.png" class="img-fluid" alt="Austria Zeichen">
+		</div>
+	</section>
+
+	<section class="row mb-5 align-items-center">
+		<div class="col-md-9">
+			<h4 class="mb-2 text-success">Sicher machen – wir sind BIO</h4>
+			<p>Es ist uns wichtig, KundenInnen über Inhaltsstoffe unserer Produkte bestmöglich zu informieren. Darum kennzeichnen wir ökologisch besonders hochwertige Produkte mit unseren hauseigenen Labels:
+
+				<ul>
+				<li>mit BIO Wirkstoffen: Diese Kennzeichnung weist auf Inhaltstoffe in Naturkosmetikprodukten hin, die biologischer Herkunft und/oder aus Wildwuchs sind.</li>
+				<li>100% zertifizierte Naturkosmetik: Mit diesem Label erkennen Sie Naturkosmetikprodukte, die nach den Standards von etablierten, internationalen Zertifizierungsstellen wie ICADA oder ECOCERT geprüft sind.</li>
+				</ul>
+				</p>
+			<a href="https://shop.styx.at/" class="btn btn-outline-success btn-sm">Zum Shop</a>
+		</div>
+		<div class="col-md-3 text-center">
+			<img src="img/cert/Bio_Wirkstoffen.png" class="img-fluid" alt="BIO Wirkstoffen">
+		</div>
+	</section>
+
+	<section class="row mb-5 align-items-center">
+		<div class="col-md-9">
+			<h4 class="mb-2 text-success">TRIGOS Regional 2019</h4>
+			<p>Die Auszeichnung für Unternehmen mit Verantwortung gibt es seit dem Jahr 2005 auch in den Bundesländern. Vorbildhafte Unternehmen haben damit die Möglichkeit, nicht nur auf Bundesebene mit dem TRIGOS Österreich,
+				sondern auch regional ausgezeichnet zu werden. STYX Naturcosmetic hat die Jury 2019 in der Kategorie
+				<br> <b>„Regionale Wertschaffung“</b> mit seinem Kooperationsprojekt <b>„Schatzsuche im Mostviertel“</b> überzeugt.</p>
+			<a href="https://trigos.at/about/trigos-regional/" class="btn btn-outline-success btn-sm">Mehr Informationen</a>
+		</div>
+		<div class="col-md-3 text-center">
+			<img src="img/cert/TRIGOS_2019.png" class="img-fluid" alt="TRIGOS 2019">
+		</div>
+	</section>
+
+	<section class="row mb-5 align-items-center">
+		<div class="col-md-9">
+			<h4 class="mb-2 text-success">European Green Award</h4>
+			<p>Der European Green Award ist ein Nonprofit-Award, der besondere Leistungen von Unternehmen und Einzelpersonen in Nachhaltigkeitsfragen ehrt. Er zeichnet grüne Produkte, Projekte, Events, Hotels, Marketing,
+				Werbung & Change Maker aus, die in Europa Exzellentes für eine nachhaltige Entwicklung leisten. Damit wollen die Initiatorinnen und Initiatoren einen essenziellen Beitrag beim Übergang Europas zu einem modernen,
+				ressourceneffizienten, fairen und wettbewerbsfähigen Wirtschaftraum der Zukunft leisten.</p>
+			<a href="#" class="btn btn-outline-success btn-sm">Mehr Informationen</a>
+		</div>
+		<div class="col-md-3 text-center">
+			<img src="img/cert/European_Green_Award.png" class="img-fluid" alt="European Green Award">
+		</div>
+	</section>
+
+</section>
--- application/views/app/zertifizierungen_en.php
+++ application/views/app/zertifizierungen_en.php
@@ -1,126 +1,68 @@
+<section class="container py-5">
+	<h2 class="text-center mb-5">Our Certifications & Awards</h2>
 
-
-<div role="main" class="main">
-	<section class="page-header page-header-modern page-header-background page-header-background-md overlay overlay-color-primary overlay-show overlay-op-9 mb-0" style="background-image: url(<?=$image1?>);">
-		<div class="container translucent-background">
-			<div class="row">
-				<div class="col align-self-center p-static text-center">
-					<h1 style="color:#000;"><?=$title?></h1>
-					<span style="color: #0a0a0a" class="sub-title"><?=$description?></span>
-				</div>
-			</div>
+	<section class="row mb-5 align-items-center">
+		<div class="col-md-9">
+			<h4 class="mb-2 text-success">ECOCERT certification</h4>
+			<p>ECOCERT is an internationally recognised organisation for the certification of food and cosmetic products. The defined quality criteria set a standard for natural and organic cosmetics.
+				STYX Naturcosmetic has been working closely with ECOCERT since 2011 to guarantee the highest quality standards.
+				<br><br>
+				During the certification, each individual product is checked. Particular attention is paid to the origin and quality of the raw materials and the processing in the company.
+				Everyone involved in the added value process – from the raw material supplier to marketing – are checked during the certification by ECOCERT.
+				In an ongoing process and continual dialogue, the whole STYX product portfolio is adapted to the standards. Already certified products can be recognised by the ECOCERT logo.</p>
+			<a href="https://www.ecocert.com/en-US/home" class="btn btn-outline-success btn-sm">More Information</a>
+		</div>
+		<div class="col-md-3 text-center">
+			<img src="img/cert/Austria_Bio_Garantie.png" class="img-fluid" alt="Austria BIO Garantie">
 		</div>
 	</section>
-	<section class="page-header bg-color-light border-bottom border-width-2 translucent-background">
-		<div class="container">
-			<div class="row">
-				<div class="col align-self-center p-static">
-					<ul class="breadcrumb d-block">
-						<li><a href="<?=BASE_URL?>">Home</a></li>
-						<li class="active"><?=$title?></li>
-					</ul>
-				</div>
-			</div>
+
+	<section class="row mb-5 align-items-center">
+		<div class="col-md-9">
+			<h4 class="mb-2 text-success">Austria organic guarantee</h4>
+			<p>Austria Bio Garantie GmbH (ABG) was founded in 1993 and the first certificatide office for organic products in Austria.
+				Their business covers inspection and certification of organic products throughout the whole food chain. The ABG is operating commissioned by the food safety authorities.
+				Products certified by ABG are identified by our code number: AT-BIO-301. Many of our handmade chocolates, as well as our hemp and poppy products, ﻿carry﻿ this organic label.</p>
+			<a href="https://www.bio-garantie.at/en/home" class="btn btn-outline-success btn-sm">More Information</a>
+		</div>
+		<div class="col-md-3 text-center">
+			<img src="img/cert/ICADA.png" class="img-fluid" alt="ICADA">
 		</div>
 	</section>
-</div>
 
-<div class="container py-4">
-
-	<div class="row">
-		<div class="col">
-			<div class="blog-posts single-post">
-
-				<article class="post post-large blog-single-post border-0 m-0 p-0">
-
-					<div class="post-content ms-0">
-
-						<h2 style="color: #aad998" class="font-weight-semi-bold">Hand in Hand mit der Natur</h2>
-
-						<div class="post-meta">
-							<h4 class="text-color-black">Traditionelles Familienunternehmen seit 1965 – im Herzen Niederösterreichs</h4>
-						</div>
-						<img src="<?=BASE_URL?>img/image/aboutStyx.jpg" class="img-fluid float-start me-4 mt-2" alt="" /><br>
-
-						<p> Inmitten geschwungener Hügel und grüner Wiesen erstreckt sich das niederösterreichische Pielachtal.
-							Ein ruhiger und beschaulicher Ort, der zum Verweilen und Träumen einlädt. Zugleich ein Ort, der die
-							Herzen all derjenigen erfreut, die sich für naturkosmetische Pflege begeistern. Hier im Innern dieser
-							einladenden Landschaft liegt Ober-Grafendorf und Kundige wissen, dort findet sich ein Platz ganz besonderer Art.
-							Dort, wo sich Tradition und Moderne vereinen, liegt die Heimat von STYX Naturcosmetic.
-							Seit 1965 und damit seit mehr als 55 Jahren widmet sich das österreichische Familienunternehmen mit mittlerweile
-							rund 60 Mitarbeitern der Herstellung von Naturkosmetik. Was sich nach erfahrungsreichem Handwerk anhört, ist bei
-							STYX Naturcosmetic noch weit mehr. Für uns führt der Prozess, in dessen Verlauf Naturkosmetik entsteht über das
-							Handwerkliche hinaus. Für den, der wahre Naturkosmetik erzeugen will, in der sich Philosophie, Qualität und
-							Leidenschaft des Unternehmens spiegeln, bedeutet dieser Prozess nicht nur Handwerk, sondern Kunst. Die Kunst
-							einer unvergleichlichen natürlichen Pflege.
-							<br>
-							Diesen hohen Anspruch möchten wir aber nicht nur in unseren Produkten erfahrbar machen. Wer sich für die Herstellung von Naturkosmetik interessiert ist herzlich eingeladen uns im Pielachtal zu besuchen. Täglich bieten wir unseren Besuchern die Möglichkeit vieles rund ums Thema Naturkosmetik authentisch und hautnah zu erfahren. Unsere hellen und verglasten Produktionshallen gewähren trotz strenger Hygieneauflagen interessante Einblicke in unsere tägliche Arbeit. Der hauseigene Naturkosmetik-Shop und ein gemütliches Bistro laden zum längeren Verweilen ein.</a>
-						</p>
-						<div class="video-container">
-							<iframe width="560" height="315" src="https://www.youtube.com/embed/atSpOLLMDEY" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-						</div>
-
-						<br><br>
-						<div class="post-meta">
-							<h4 class="text-color-black">Staatliche Auszeichnung (2003)</h4>
-						</div>
-						<img src="<?=BASE_URL?>img/image/Staatliche-Auszeichnung-rgb.jpg" width="200" class="img-fluid float-end me-4 mt-2" alt="Staatliche-Auszeichnung-rgb.jpg" /><br>
-
-						<p>Staatliche Auszeichnungen in Österreich werden durch den Bundesminister für Wirtschaft an Unternehmen verliehen,
-							die sich durch außergewöhnliche Leistungen um die österreichische Wirtschaft Verdienste erworben haben.
-							<br>Seit 2003 sind wir stolz, diese Auszeichnung tragen zu dürfen.
-						</p>
-						<br>
-						<div class="post-block mt-5 post-share">
-							<a href="https://www.staatswappen.at/" class="btn btn-primary w-50 mb-2">Mehr Information</a>
-
-							<!-- Go to www.addthis.com/dashboard to customize your tools -->
-							<div class="addthis_inline_share_toolbox"></div>
-							<script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-60ba220dbab331b0"></script>
-
-						</div>
-						<br>
-						<h4>Green Brand Austria Gütesiegel (2022/2023)</h4>
-						<img src="<?=BASE_URL?>img/image/Green Brands 2022_2023 STYX Naturcosmetic.jpg" width="200" class="img-fluid float-end me-4 mt-2" alt="Green Brands 2022_2023 STYX Naturcosmetic.jpg" /><br>
-
-						<p> Für unseren wertvollen Beitrag zum Umwelt- und Klimaschutz. Green Brands honoriert darüber Hinaus auch die
-							Verpflichtung zur Nachhaltigkeit und zur ökologischer Verantwortung
-						</p>
-						<br><br>
-						<div class="post-block mt-5 post-share">
-							<a href="https://https://green-brands.org/" class="btn btn-primary w-50 mb-2">Mehr Information</a>
-
-							<!-- Go to www.addthis.com/dashboard to customize your tools -->
-							<div class="addthis_inline_share_toolbox"></div>
-							<script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-60ba220dbab331b0"></script>
-
-						</div>
-
-						<br><br>
-
-						<h4>Klimabündnisbetrieb Österreich (2018)</h4>
-						<img src="<?=BASE_URL?>img/image/kbu_logos_betrieb-rgb.jpg" width="200" class="img-fluid float-end me-4 mt-2" alt="" /><br>
-
-						<p> Mit Freude sind wir die freiwillige Verpflichtung für aktive Maßnahmen zum nachhaltigen Klimaschutz eingegangen.
-							<br>
-							Das Klimabündnis ist eine globale Partnerschaft zum Schutz des Klimas und verbindet mittlerweile
-							<br> mehr als 1.700 Gemeinden in 26 Ländern Europas.
-						</p>
-						<br><br>
-						<div class="post-block mt-5 post-share">
-							<a href="https://www.klimabuendnis.at/" class="btn btn-primary w-50 mb-2">Mehr Information</a>
-
-							<!-- Go to www.addthis.com/dashboard to customize your tools -->
-							<div class="addthis_inline_share_toolbox"></div>
-							<script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-60ba220dbab331b0"></script>
-
-						</div>
-
-					</div>
-				</article>
-
-			</div>
+	<section class="row mb-5 align-items-center">
+		<div class="col-md-9">
+			<h4 class="mb-2 text-success">ICADA</h4>
+			<p>ICADA (International Cosmetic and Device Association) is a European service and lobby association in the field of preparative cosmetics, apparative cosmetics, natural detergents and cleansers
+				as well as food supplements. ICADA, with its more than 20 years of experience, offers a premium label for organic and natural cosmetics, which is particularly strict and reliable due to the
+				double control system, and close to nature due to the permission of relatively few additives (short positive list).</p>
+			<a href="https://www.icada.eu/whoweare/" class="btn btn-outline-success btn-sm">More Information</a>
 		</div>
-	</div>
+		<div class="col-md-3 text-center">
+			<img src="img/cert/ECOCERT.png" class="img-fluid" alt="ECOCERT COSMOS">
+		</div>
+	</section>
 
-</div>
+	<section class="row mb-5 align-items-center">
+		<div class="col-md-9">
+			<h4 class="mb-2 text-success">Vegan</h4>
+			<p>Our vegan logo allows you to see at a glance which STYX products are 100% free from animal-based ingredients.</p>
+		</div>
+		<div class="col-md-3 text-center">
+			<img src="img/cert/Vegan.png" class="img-fluid" alt="VEGAN">
+		</div>
+	</section>
+
+	<section class="row mb-5 align-items-center">
+		<div class="col-md-9">
+			<h4 class="mb-2 text-success">IHTN – Cruelty free cosmetics</h4>
+			<p>STYX is a member of the Internationalen Herstellerverband für tierschutzgeprüfte Naturkosmetik, Kosmetik und Naturwaren e.V.
+				[International Manufactory Association against Animal Testing in Cosmetics] (IHTN). This guarantees that the product range is manufactured without animal testing.</p>
+			<a href="https://shop.styx.at" class="btn btn-outline-success btn-sm">Our Certificate</a>
+		</div>
+		<div class="col-md-3 text-center">
+			<img src="img/cert/Trusted_Shops.png" class="img-fluid" alt="Trusted Shops">
+		</div>
+	</section>
+
+	<section class="r
