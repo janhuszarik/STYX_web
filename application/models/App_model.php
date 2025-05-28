48000f4ca701765bf380dc615057e28d71f010a5
Jan Huszarik <assist04@styx.at>
2025-05-28 16:08:50 +0200

    fg

--- application/models/App_model.php
+++ application/models/App_model.php
@@ -45,23 +45,22 @@ class app_model extends CI_Model
 	function getAllActiveNews()
 	{
 		$this->db->select('*');
-		$this->db->where('active', '1');
-		$this->db->where('lang', language());
-		$this->db->where('start_date <=', date('Y-m-d H:i:s'));
-		$this->db->where('end_date >=', date('Y-m-d H:i:s'));
-		return $this->db->get('news')->result(); // upravené
-	}
-	function getAllNews_article($id)
-	{
-		$this->db->select('*');
 		$this->db->from('news');
 		$this->db->where('active', '1');
-		$this->db->where('id', $id);
 		$this->db->where('lang', language());
 		$this->db->where('start_date IS NULL OR start_date <=', date('Y-m-d'));
 		$this->db->where('end_date IS NULL OR end_date >=', date('Y-m-d'));
-		return $this->db->get()->row();
+		$this->db->order_by('start_date', 'DESC');
+		return $this->db->get()->result();
 	}
+	function getAllNews_article($id)
+{
+	$this->db->select('*');
+	$this->db->where('active', '1');
+	$this->db->where('id', $id);
+	$this->db->where('lang', language());
+	return $this->db->get('news')->row(); // upravené
+}
 
 	function getAllActiveProduct()
 	{
