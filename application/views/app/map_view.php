e3faf13c2d2437a12741434c986b9d230e41590f
Janco1002 <80489863+Janco1002@users.noreply.github.com>
2025-06-14 15:49:29 +0200

    dd

--- application/views/app/map_view.php
+++ application/views/app/map_view.php
@@ -15,7 +15,7 @@
 		<p class="text-muted">Übersicht unserer Filialen auf der Karte</p>
 		<p>In den folgenden Geschäften, Märkten und Bio-Läden können Sie unsere STYX-Produkte in Österreich sowie in Deutschland kaufen.<br>
 			Bitte beachten Sie, dass nicht alle Produkte in jedem Shop verfügbar sind. <br> Alle Produkte finden Sie in unserem
-			<a href="https://www.styx-shop.at">Online-Shop</a>.
+			<a href="https://shop.styx.at/">Online-Shop</a>.
 		</p>
 		<div class="map-container">
 			<div id="map" class="map-area"></div>
