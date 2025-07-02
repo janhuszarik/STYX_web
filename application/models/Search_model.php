dcf8095fe7349644ae3b122a8d8c285a1615c275
Jan Huszarik <assist04@styx.at>
2025-07-02 10:50:54 +0200

    zatiaľ tiež ok

--- application/models/Search_model.php
+++ application/models/Search_model.php
@@ -9,30 +9,75 @@ class Search_model extends CI_Model {
 	}
 
 	public function search($query) {
-		$orig_query = $query; // pre PHP filter nižšie
+		$orig_query = $query;
 		$query = $this->db->escape_like_str($query);
 		$results = [];
 
-		// --- Articles (title a content) ---
-		$this->db->select('IFNULL(empfohlen_name1, CONCAT("Article ", id)) as title, content1 as content, empfohlen_url1 as url, "article" as type, id');
+		// ========================
+		// ARTICLES – úplný výber
+		// ========================
+		$this->db->select('
+			id, 
+			category_id, subcategory_id, lang, slug, title, subtitle, text, keywords, content1, meta, 
+			gallery_id, image, image_title, slug_title, empfohlen_name1, empfohlen_name2, empfohlen_name3, 
+			empfohlen_url1, empfohlen_url2, empfohlen_url3, 
+			product_set1_product1_name, product_set1_product1_description, product_set1_product2_name, product_set1_product2_description, 
+			product_set1_product3_name, product_set1_product3_description,
+			product_set2_product1_name, product_set2_product1_description, product_set2_product2_name, product_set2_product2_description, 
+			product_set2_product3_name, product_set2_product3_description,
+			"article" as type
+		');
 		$this->db->from('articles');
 		$this->db->group_start();
-		$this->db->like('empfohlen_name1', $query);
+		// Všetky textové polia pre dôkladné vyhľadávanie
+		$this->db->like('title', $query);
+		$this->db->or_like('subtitle', $query);
+		$this->db->or_like('text', $query);
+		$this->db->or_like('keywords', $query);
 		$this->db->or_like('content1', $query);
+		$this->db->or_like('meta', $query);
+		$this->db->or_like('slug', $query);
+		$this->db->or_like('slug_title', $query);
+		$this->db->or_like('empfohlen_name1', $query);
+		$this->db->or_like('empfohlen_name2', $query);
+		$this->db->or_like('empfohlen_name3', $query);
+		$this->db->or_like('empfohlen_url1', $query);
+		$this->db->or_like('empfohlen_url2', $query);
+		$this->db->or_like('empfohlen_url3', $query);
+		// Produkty v článkoch
+		$this->db->or_like('product_set1_product1_name', $query);
+		$this->db->or_like('product_set1_product1_description', $query);
+		$this->db->or_like('product_set1_product2_name', $query);
+		$this->db->or_like('product_set1_product2_description', $query);
+		$this->db->or_like('product_set1_product3_name', $query);
+		$this->db->or_like('product_set1_product3_description', $query);
+		$this->db->or_like('product_set2_product1_name', $query);
+		$this->db->or_like('product_set2_product1_description', $query);
+		$this->db->or_like('product_set2_product2_name', $query);
+		$this->db->or_like('product_set2_product2_description', $query);
+		$this->db->or_like('product_set2_product3_name', $query);
+		$this->db->or_like('product_set2_product3_description', $query);
 		$this->db->group_end();
 		$articles_results = $this->db->get();
 		$articles_results = $articles_results !== FALSE ? $articles_results->result_array() : [];
 		$results = array_merge($results, $articles_results);
 
-		// --- Article categories (title) ---
-		$this->db->select('ac.name as title, ac.id as id, IFNULL(m.url, ac.id) as url, "article_category" as type');
+		// ========================
+		// ARTICLE_CATEGORIES
+		// ========================
+		$this->db->select('ac.id, ac.name as title, ac.slug, ac.lang, ac.active, ac.keywords, ac.description as description, IFNULL(m.url, ac.id) as url, "article_category" as type');
 		$this->db->from('article_categories ac');
 		$this->db->join('menu m', 'm.name = ac.name', 'left');
+		$this->db->group_start();
 		$this->db->like('ac.name', $query);
+		$this->db->or_like('ac.slug', $query);
+		$this->db->or_like('ac.keywords', $query);
+		$this->db->or_like('ac.description', $query);
+		$this->db->group_end();
 		$article_categories_results = $this->db->get();
 		$article_categories_results = $article_categories_results !== FALSE ? $article_categories_results->result_array() : [];
 		foreach ($article_categories_results as &$cat) {
-			if (strpos($cat['url'], 'http') === 0) {
+			if (strpos($cat['url'], 'http') !== false) {
 				$cat['url'] = $cat['url'];
 			} else {
 				$cat['url'] = base_url() . 'de/' . str_replace(' ', '-', strtolower($cat['title']));
@@ -40,23 +85,48 @@ class Search_model extends CI_Model {
 		}
 		$results = array_merge($results, $article_categories_results);
 
-		// --- Article sections (title a content) ---
-		$this->db->select('IFNULL(button_name, SUBSTRING_INDEX(content, "<", 1)) as title, content as content, IFNULL(article_link, external_url) as url, "article_section" as type, id');
+		// ========================
+		// ARTICLE_SECTIONS
+		// ========================
+		$this->db->select('id, button_name as title, content, article_link as url, "article_section" as type');
 		$this->db->from('article_sections');
 		$this->db->group_start();
 		$this->db->like('button_name', $query);
 		$this->db->or_like('content', $query);
+		$this->db->or_like('article_link', $query);
 		$this->db->group_end();
 		$article_sections_results = $this->db->get();
 		$article_sections_results = $article_sections_results !== FALSE ? $article_sections_results->result_array() : [];
 		$results = array_merge($results, $article_sections_results);
 
-		// --- BestProduct (title a content) ---
-		$this->db->select('name as title, aktion_name as content, url as url, "best_product" as type, id');
+		// ========================
+		// NEWS
+		// ========================
+		$this->db->select('id, name as title, name1, content, buttonUrl as url, "news" as type');
+		$this->db->from('news');
+		$this->db->group_start();
+		$this->db->like('name', $query);
+		$this->db->or_like('name1', $query);
+		$this->db->or_like('content', $query);
+		$this->db->or_like('buttonUrl', $query);
+		$this->db->group_end();
+		$news_results = $this->db->get();
+		$news_results = $news_results !== FALSE ? $news_results->result_array() : [];
+		foreach ($news_results as &$news) {
+			$news['url'] = !empty($news['url']) ? $news['url'] : '#';
+		}
+		$results = array_merge($results, $news_results);
+
+
+		// ========================
+		// BEST PRODUCT
+		// ========================
+		$this->db->select('id, name as title, aktion_name as content, url, "best_product" as type');
 		$this->db->from('bestProduct');
 		$this->db->group_start();
 		$this->db->like('name', $query);
 		$this->db->or_like('aktion_name', $query);
+		$this->db->or_like('url', $query);
 		$this->db->group_end();
 		$best_product_results = $this->db->get();
 		$best_product_results = $best_product_results !== FALSE ? $best_product_results->result_array() : [];
@@ -65,12 +135,16 @@ class Search_model extends CI_Model {
 		}
 		$results = array_merge($results, $best_product_results);
 
-		// --- Locations (title a content) ---
-		$this->db->select('name as title, CONCAT(opening_hours, " ", holidays) as content, website as url, "location" as type, id');
+		// ========================
+		// LOCATIONS
+		// ========================
+		$this->db->select('id, name as title, CONCAT(opening_hours, " ", holidays) as content, website as url, "location" as type');
 		$this->db->from('locations');
 		$this->db->group_start();
 		$this->db->like('name', $query);
-		$this->db->or_like('CONCAT(opening_hours, " ", holidays)', $query);
+		$this->db->or_like('opening_hours', $query);
+		$this->db->or_like('holidays', $query);
+		$this->db->or_like('website', $query);
 		$this->db->group_end();
 		$locations_results = $this->db->get();
 		$locations_results = $locations_results !== FALSE ? $locations_results->result_array() : [];
@@ -79,8 +153,10 @@ class Search_model extends CI_Model {
 		}
 		$results = array_merge($results, $locations_results);
 
-		// --- Neuigkeiten subcategories (title) ---
-		$this->db->select('name as title, id as url, "neuigkeiten_subcategory" as type, id');
+		// ========================
+		// NEUIGKEITEN SUBCATEGORIES
+		// ========================
+		$this->db->select('id, name as title, "" as content, "" as url, "neuigkeiten_subcategory" as type');
 		$this->db->from('neuigkeiten_subcategories');
 		$this->db->like('name', $query);
 		$neuigkeiten_results = $this->db->get();
@@ -90,37 +166,34 @@ class Search_model extends CI_Model {
 		}
 		$results = array_merge($results, $neuigkeiten_results);
 
-		// --- News (title a content) ---
-		$this->db->select('name as title, content as content, buttonUrl as url, "news" as type, id');
-		$this->db->from('news');
-		$this->db->group_start();
-		$this->db->like('name', $query);
-		$this->db->or_like('content', $query);
-		$this->db->group_end();
-		$news_results = $this->db->get();
-		$news_results = $news_results !== FALSE ? $news_results->result_array() : [];
-		foreach ($news_results as &$news) {
-			$news['url'] = !empty($news['url']) ? $news['url'] : '#';
-		}
-		$results = array_merge($results, $news_results);
-
-		// --- Unikátne výsledky (ID alebo URL) ---
+		// ========================
+		// DEDUPLIKÁCIA (unikátne záznamy)
+		// ========================
 		$unique_results = [];
 		foreach ($results as $result) {
-			$key = $result['type'] . '|' . ($result['type'] === 'article_section' ? $result['id'] : $result['url']);
-			if (!isset($unique_results[$key]) && !empty($result['url']) && $result['url'] !== '#') {
+			$key = $result['type'] . '|' . (isset($result['id']) ? $result['id'] : (isset($result['url']) ? $result['url'] : ''));
+			if (!isset($unique_results[$key])) {
 				$unique_results[$key] = $result;
 			}
 		}
 		$results = array_values($unique_results);
 
-		// --- Filter: presná zhoda celej frázy v title alebo content (case-insensitive) ---
+		// ========================
+		// PHP FULLTEXT filter – presná zhoda v textoch (case-insensitive)
+		// ========================
 		$final_results = [];
 		$lower_query = mb_strtolower($orig_query, 'UTF-8');
 		foreach ($results as $res) {
 			$haystack = '';
-			if (!empty($res['title'])) $haystack .= ' ' . $res['title'];
-			if (!empty($res['content'])) $haystack .= ' ' . $res['content'];
+			foreach ($res as $field => $value) {
+				if (in_array($field, ['title', 'subtitle', 'text', 'keywords', 'content', 'content1', 'meta', 'description', 'empfohlen_name1', 'empfohlen_name2', 'empfohlen_name3', 'slug', 'slug_title', 'aktion_name', 'name', 'opening_hours', 'holidays'])) {
+					$haystack .= ' ' . $value;
+				}
+				// Dynamicky pridaj všetky product_set polia
+				if (strpos($field, 'product_set') === 0) {
+					$haystack .= ' ' . $value;
+				}
+			}
 			$haystack = mb_strtolower($haystack, 'UTF-8');
 			if (mb_strpos($haystack, $lower_query) !== false) {
 				$final_results[] = $res;
@@ -128,12 +201,24 @@ class Search_model extends CI_Model {
 		}
 		$results = $final_results;
 
-		// --- Relevancia (zachovaj sort ak chceš) ---
+		// ========================
+		// Zoradenie podľa relevancie (match v title > content > description > ostatné)
+		// ========================
 		usort($results, function($a, $b) use ($lower_query) {
-			$a_match = (mb_strpos(mb_strtolower($a['title'] ?? '', 'UTF-8'), $lower_query) !== false ? 2 : 0)
-				+ (mb_strpos(mb_strtolower($a['content'] ?? '', 'UTF-8'), $lower_query) !== false ? 1 : 0);
-			$b_match = (mb_strpos(mb_strtolower($b['title'] ?? '', 'UTF-8'), $lower_query) !== false ? 2 : 0)
-				+ (mb_strpos(mb_strtolower($b['content'] ?? '', 'UTF-8'), $lower_query) !== false ? 1 : 0);
+			$a_match = 0;
+			$b_match = 0;
+			foreach (['title', 'subtitle', 'name'] as $f) {
+				if (isset($a[$f]) && mb_strpos(mb_strtolower($a[$f], 'UTF-8'), $lower_query) !== false) $a_match += 3;
+				if (isset($b[$f]) && mb_strpos(mb_strtolower($b[$f], 'UTF-8'), $lower_query) !== false) $b_match += 3;
+			}
+			foreach (['text', 'content', 'content1', 'aktion_name'] as $f) {
+				if (isset($a[$f]) && mb_strpos(mb_strtolower($a[$f], 'UTF-8'), $lower_query) !== false) $a_match += 2;
+				if (isset($b[$f]) && mb_strpos(mb_strtolower($b[$f], 'UTF-8'), $lower_query) !== false) $b_match += 2;
+			}
+			foreach (['description', 'meta', 'keywords'] as $f) {
+				if (isset($a[$f]) && mb_strpos(mb_strtolower($a[$f], 'UTF-8'), $lower_query) !== false) $a_match += 1;
+				if (isset($b[$f]) && mb_strpos(mb_strtolower($b[$f], 'UTF-8'), $lower_query) !== false) $b_match += 1;
+			}
 			return $b_match - $a_match;
 		});
 
--- application/views/app/search_results.php
+++ application/views/app/search_results.php
@@ -1,15 +1,43 @@
+<?php
+// Pomocná funkcia na generovanie URL článku podľa slugu
+if (!function_exists('get_article_url')) {
+    function get_article_url($result) {
+        if (!empty($result['slug'])) {
+            return base_url() . $result['slug'];
+        }
+        // fallback – ak nemáš slug
+        return '#';
+    }
+}
+?>
+
 <?php if (!empty($results) && is_array($results)): ?>
     <div class="search-results">
         <h2 class="mb-4">Suchergebnisse für "<?= htmlspecialchars($query) ?>"</h2>
         <div class="search-result-list">
             <?php foreach ($results as $result): ?>
                 <div class="search-result-item mb-3 p-3 border rounded" style="background-color: #ffffff; box-shadow: 0 2px 4px rgba(0,0,0,0.1);">
-                    <h3 class="mb-1">
-                        <a href="<?= htmlspecialchars($result['url']) ?>" class="text-decoration-none text-primary" target="_blank" <?= empty($result['url']) || $result['url'] === '#' ? 'style="pointer-events: none; color: #6c757d;"' : '' ?>>
-                            <?= htmlspecialchars(trim($result['title']) ?: 'ID ' . $result['id']) ?>
-                        </a>
-                    </h3>
-                    <?php if ($result['type'] == 'article_category'): ?>
+
+                    <?php if ($result['type'] == 'article'): ?>
+                        <h3 class="mb-1">
+                            <a href="<?= htmlspecialchars(get_article_url($result)) ?>"
+                               class="text-decoration-none text-primary" target="_blank">
+                                <?= htmlspecialchars(trim($result['title']) ?: 'ID ' . $result['id']) ?>
+                            </a>
+                        </h3>
+                        <small class="text-secondary d-block mt-1">
+                            <i class="fas fa-globe me-1"></i>
+                            <a href="<?= htmlspecialchars(get_article_url($result)) ?>" target="_blank">
+                                <?= htmlspecialchars(get_article_url($result)) ?>
+                            </a>
+                        </small>
+                    <?php elseif ($result['type'] == 'article_category'): ?>
+                        <h3 class="mb-1">
+                            <a href="<?= htmlspecialchars($result['url']) ?>"
+                               class="text-decoration-none text-primary" target="_blank">
+                                <?= htmlspecialchars(trim($result['title']) ?: 'ID ' . $result['id']) ?>
+                            </a>
+                        </h3>
                         <small class="text-secondary d-block mt-1">
                             <i class="fas fa-globe me-1"></i>
                             <a href="<?= htmlspecialchars($result['url']) ?>" target="_blank"><?= htmlspecialchars($result['url']) ?></a>
@@ -18,6 +46,13 @@
                             <p class="mb-1 text-muted small"><?= htmlspecialchars($result['description']) ?></p>
                         <?php endif; ?>
                     <?php else: ?>
+                        <h3 class="mb-1">
+                            <a href="<?= htmlspecialchars($result['url']) ?>"
+                               class="text-decoration-none text-primary" target="_blank"
+                                <?= empty($result['url']) || $result['url'] === '#' ? 'style="pointer-events: none; color: #6c757d;"' : '' ?>>
+                                <?= htmlspecialchars(trim($result['title']) ?: 'ID ' . $result['id']) ?>
+                            </a>
+                        </h3>
                         <small class="text-secondary">
                             <i class="fas fa-folder-open me-1"></i>
                             <?= $generate_path($result) ? $generate_path($result) : 'Unbekannter Pfad' ?>
